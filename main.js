9f50f32dbaf602acf227b81388686d1082409e6e
EpicFn <collegeruled3@naver.com>
2024-02-27 20:36:30 +0900

    Read 기능 구현 완료

--- main.js
+++ main.js
@@ -6,20 +6,30 @@ var template = require('./template');
 // app 구축
 
 app.use(express.static('css templates'));
+app.use(express.static('Data'));
+
 
 app.get('/', function(req,res){
     res.send(template.board(1));
 });
 
-app.get('/:page', function(req,res){
-    var params = req.params;
+app.get('/board', function(req,res){
+    var pageNum = req.query.page;
+
     
-    if(params.page == '1'){
+    if(pageNum == '1'){
         res.redirect(`/`);
     }
     else{
-        res.send(template.board(Number(params.page)));
+        res.send(template.board(Number(pageNum)));
     }
 })
+
+app.get('/post', function(req, res){
+    var id = req.query.id;
+
+    res.send(template.post(id));
+})
+
 
 app.listen(3000);
--- template.js
+++ template.js
@@ -17,7 +17,7 @@ var template = {
 
             for(var i =0; i<4; i++){
                 if(((page-1)*4 + i) < files.length){
-                    post[i] = `<li class="content1"><a href='/post/${files[(page-1)*4 + i]}'>${files[(page-1)*4 + i]}</a></li>`;
+                    post[i] = `<li class="content1"><a href='/post?id=${files[(page-1)*4 + i]}'>${files[(page-1)*4 + i]}</a></li>`;
                 }
                 else
                     break;
@@ -35,20 +35,20 @@ var template = {
         var link_button = ['', '', ''];
 
         if(page > 3)
-            back_link = `/${page-3}`;
+            back_link = `/board?page=${page-3}`;
         else
-            back_link = `/1`;
+            back_link = `/board?page=1`;
 
 
         if(page+3 > max_page)
-            post_link = `/${max_page}`;
+            post_link = `/board?page=${max_page}`;
         else
-            post_link = `/${page+3}`;
+            post_link = `/board?page=${page+3}`;
 
         for(var i=0; i<3; i++){
             if(page+i <= max_page){
                 link_button[i] = `
-            <li class="page-item"><a class="page-link" href="/${page+i}">${page+i}</a></li>
+            <li class="page-item"><a class="page-link" href="/board?page=${page+i}">${page+i}</a></li>
             `
             }
         }
@@ -103,9 +103,59 @@ var template = {
         </html>
         `;
     },
-    post : function(){
+    post : function(id){
+
+        var title = id;
+        var line = 'null';
+
+        var fs = require('fs')
+        const filepath = `./Data/${id}`;
+
+        try {
+            line = fs.readFileSync(filepath, 'utf8');
+        }   catch(err){
+            console.error(err);
+        }
+        
+
         return `
-        post
+        <!doctype html>
+        <html lang="en">
+            <head>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <title>post</title>
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+                <link rel="stylesheet" href="postStyle.css" >
+            </head>
+            <body>
+                <header>
+                    <nav class="navbar bg-primary" data-bs-theme="dark">
+                        <div class="container-fluid">
+                            <a class="navbar-brand" href="/">게시판!!</a>
+                            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
+                            <div class="collapse navbar-collapse" id="navbarNav">
+                                <ul class="navbar-nav">
+                                    <li class="nav-item">
+                                        <a class="nav-link active" aria-current="page" href="#">수정</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">삭제</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </nav>
+                </header>
+                <main>
+                    <h1>${title}</h1>
+                    <article>${line}</article>
+                </main>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+            </body>
+        </html>
         `
     }
 };
